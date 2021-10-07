8314227431223771d9be4c899d968eab8f14b420
Kaylinkk <kyulim0429@163.com>
2021-10-07 21:19:45 +0800

    CSS:signUp Page

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./NavBar.module.css";
-import { ReactComponent as Logo } from "./img/logo.svg"
+
+
 
 function NavBar() {
 
@@ -15,9 +16,7 @@ function NavBar() {
 
 
             <button onClick={onHandleClick} className={styles.signup}>Sign Up</button>
-            <button className={styles.button} >
-                {/* <Toggle width="40" height="40" /> */}
-            </button>
+
         </div>
 
     )
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,25 +1,39 @@
 import React, { useState, useEffect } from "react";
-import styles from "./Login.module.css";
+import styles from "./Signup.module.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Input from "./Input";
+import { ReactComponent as Mail } from "../img/mail.svg";
 import { ReactComponent as User } from "../img/person.svg";
 import { ReactComponent as Password } from "../img/password.svg";
-function Login(props) {
-  document.body.style.backgroundColor = "#9C9CD3";
+function SignUp(props) {
+  document.body.style.background =
+    "linear-gradient(rgba(179,157,219,1) 16%, rgba(221,234,249,1) 100%) fixed";
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("username", username);
     console.log("password", password);
+    console.log("eamil", email);
   };
 
   return (
     <>
       <div className={styles.box}>
         <form onSubmit={handleSubmit}>
-          <h1 className={styles.title}>Sign Up</h1>
+          <h1 className={styles.h1}>Sign Up</h1>
+
+          <Input
+            Icon={Mail}
+            className={styles.inputbox}
+            value={email}
+            setValue={setEmail}
+            placeholder="e-mail"
+            type="text"
+            required
+          />
 
           <Input
             Icon={User}
@@ -41,6 +55,16 @@ function Login(props) {
             required
           />
 
+          <Input
+            Icon={Password}
+            className={styles.inputbox}
+            setValue={setPassword}
+            value={password}
+            placeholder="confirm password"
+            type="text"
+            required
+          />
+
           <Link to="/todo">
             <button className={styles.button} type="button">
               Create Account
@@ -52,4 +76,4 @@ function Login(props) {
   );
 }
 
-export default Login;
+export default SignUp;
