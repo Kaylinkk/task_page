2e9a14b37ee2d497c2415d68b248d39fa6637e5e
Kaylinkk <kyulim0429@163.com>
2021-10-08 19:42:34 +0800

    CSS:main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from "react";
 import styles from "./MainPage.module.css";
 
 function MainPage() {
-  document.body.style.backgroundColor = "#F1F1F9";
   const handleonClick = () => {
     document.location.href = "/login";
   };
 
   return (
     <>
-      <div className={styles.buttonBox}>
-        <button onClick={handleonClick} className={styles.button}>
-          Log In
-        </button>
+      <div className={styles.backgroundContainer}>
+        <ul className={styles.material}>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <div className={styles.buttonBox}>
+          <button onClick={handleonClick} className={styles.button}>
+            Log In
+          </button>
+        </div>
       </div>
     </>
   );
