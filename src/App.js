867da777c749b7bd376a2df212648ca26c8c5102
Kaylinkk <kyulim0429@163.com>
2021-10-13 12:20:01 +0800

    CSS:made one file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter } from "react-router-dom";
-import NavBar from './components/NavBar';
 import Todo from './components/Todo';
 import Login from './components/Login';
 import MainPage from "./components/MainPage";
 import SignUp from "./components/SignUp";
 import "./components/App.css"
-
+import { useMediaQuery } from 'react-responsive'
 
 
 
 
 function App() {
+  const isMobileDevice = useMediaQuery({
+    query: "(min-device-width: 480px)",
+  });
 
+  const isTabletDevice = useMediaQuery({
+    query: "(min-device-width: 768px)",
+  });
+
+  const isLaptop = useMediaQuery({
+    query: "(min-device-width: 1024px)",
+  });
+
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1200px)",
+  });
+
+  const isBigScreen = useMediaQuery({
+    query: "(min-device-width: 1201px )",
+  });
 
   return (
+
+
     <>
       <Router>
         <Switch>
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-
 function MainPage() {
   const handleonClick = () => {
     document.location.href = "/login";
